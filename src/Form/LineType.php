38fd572404a3f544c4adcb1e1b3ed416a38ee4f2
waseem1104 <waseem11@hotmail.fr>
2021-12-21 01:02:59 +0100

    Add stations.json and read json file to get name stations

--- src/Form/LineType.php
+++ src/Form/LineType.php
@@ -4,21 +4,33 @@ namespace App\Form;
 
 use App\Entity\Line;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class LineType extends AbstractType
-{
+class LineType extends AbstractType{
+
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $file = '../public/stations.json';
+        $data = file_get_contents($file);
+        $obj = json_decode($data,true);
+
+        foreach ($obj as $value){
+           $stations[$value['Nom_Gare']] = $value['Nom_Gare'];
+        }
+        asort($stations);
+
         $builder
-            ->add('longitude_departure')
-            ->add('latitude_departure')
-            ->add('longitude_arrival')
-            ->add('latitude_arrival')
-            ->add('name_station_departure')
-            ->add('name_station_arrival')
-        ;
+            ->add('name_station_departure',ChoiceType::class, [
+                'choices' => $stations
+            ])
+            ->add('name_station_arrival',ChoiceType::class, [
+                'choices' => $stations
+            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
