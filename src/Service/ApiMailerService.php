dd53f8892784547d0aa524d81aec1bbc4eb09557
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2022-03-31 18:07:32 +0200

    User - reset password

--- src/Service/ApiMailerService.php
+++ src/Service/ApiMailerService.php
@@ -3,13 +3,14 @@
 namespace App\Service;
 
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Address;
 
 class ApiMailerService
 {
     static function send_email($to, $subject, $template, $context)
     {
         $email = (new TemplatedEmail())
-            ->from('no-reply@express.com')
+            ->from(new Address('no-reply@express.com', 'Express'))
             ->to($to)
             ->htmlTemplate('Emails/'.$template)
             ->context($context)
